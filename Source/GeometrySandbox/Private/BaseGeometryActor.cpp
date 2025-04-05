11b941d06a2b553018d4a585fb0ce8c1cc57c62d
Evgeny0420 <evgeniy_karpov91@inbox.ru>
2025-04-05 21:11:49 +0300

    study/level_1 UE_LOG, types, FString, UPROPERTY, FTransform, USTRUCT, UEMUM

--- Source/GeometrySandbox/Private/BaseGeometryActor.cpp
+++ Source/GeometrySandbox/Private/BaseGeometryActor.cpp
@@ -11,6 +11,8 @@ ABaseGeometryActor::ABaseGeometryActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMesh");
+	SetRootComponent(BaseMesh);
 }
 
 // Called when the game starts or when spawned
@@ -18,21 +20,33 @@ void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString Name = "John Conor";
+	InitialLocation = GetActorLocation();
 
-	printTypes();
-
-	FString WeaponsNumStr = "Weapons Num - " + FString::FromInt(WeaponsNum);
-	FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
-	FString IsWeaponStr = "Is Weapon = " + FString(IsWeapon ? "true" : "false");
-
-	FString Stat = FString::Printf(TEXT("\n All stat \n %s \n %s \n %s"), *WeaponsNumStr, *HealthStr, *IsWeaponStr);
+	// printTypes();
+	// printTransform();
 }
 
 // Called every frame
 void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	switch (GeometryData.MoveType)
+	{
+	case EMovementType::Sin:
+	{
+		FVector CurrentLocation = GetActorLocation();
+		float time = GetWorld()->GetTimeSeconds();
+		CurrentLocation.Z = InitialLocation.Z + GeometryData.Amplitude * FMath::Sin(GeometryData.Frequency * time);
+
+		SetActorLocation(CurrentLocation);
+	}
+		break;
+	case EMovementType::Static:
+		break;
+	default:
+		break;
+	}
 
 }
 
@@ -43,5 +57,20 @@ void ABaseGeometryActor::printTypes()
 	UE_LOG(LogBaseGeometry, Warning, TEXT("Health: %f"), Health);
 	UE_LOG(LogBaseGeometry, Warning, TEXT("isDead: %d"), isDead);
 	UE_LOG(LogBaseGeometry, Warning, TEXT("IsWeapon: %d"), IsWeapon);
+}
+
+void ABaseGeometryActor::printTransform()
+{
+	FTransform Transform = GetActorTransform();
+	FVector Location = Transform.GetLocation();
+	FRotator Rotation = Transform.Rotator();
+	FVector Scale = Transform.GetScale3D();
+
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Transform %s"), *Transform.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Location %s"), *Location.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Rotation %s"), *Rotation.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Scale %s"), *Scale.ToString());
+
+	UE_LOG(LogBaseGeometry, Error, TEXT("Human transform %s"), *Transform.ToHumanReadableString());
 }
 
